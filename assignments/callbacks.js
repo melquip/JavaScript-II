8ff90c3d9c6a44c5d79ef11e6589bd9a85ce0a62
Melquisedeque Pereira <melqui1@live.com.pt>
2019-08-20 12:54:10 +0100

    Stretch: Finished Callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -40,35 +40,40 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
-  return cb(arr.length);
+	// getLength passes the length of the array into the callback.
+	return cb(arr.length);
 }
 
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
-  return cb(arr[arr.length - 1]);
+	// last passes the last item of the array into the callback.
+	return cb(arr[arr.length - 1]);
 }
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
-  return cb(x + y);
+	// sumNums adds two numbers (x, y) and passes the result to the callback.
+	return cb(x + y);
 }
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
-  return cb(x * y);
+	// multiplyNums multiplies two numbers and passes the result to the callback.
+	return cb(x * y);
 }
 
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
-  return cb(list.find((listItem) => listItem === item) === item);
+	// contains checks if an item is present inside of the given array/list.
+	// Pass true to the callback if it is, otherwise pass false.
+	//list.find((listItem) => listItem === item) === item
+	return cb(list.includes(item));
 }
 
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
+	// removeDuplicates removes all duplicate values from the given array.
+	// Pass the duplicate free array to the callback function.
+	// Do not mutate the original array.
+	// can't use unique.push(arrItem)
+	return cb(array.reduce((unique, arrItem) => unique.includes(arrItem) ? unique : [...unique, arrItem], []));
 }
+
+// removeDuplicates([1, 1, 2, 2, 3, 3, 3], (arr) => console.log(arr)); // test
